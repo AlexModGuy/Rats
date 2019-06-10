5217d043a2065e0b14cb7d873aa885183403604d
Alexthe666 <alex.rowlands@cox.net>
2019-06-10 10:06:53 -0700

    1.0.1

--- src/main/java/com/github/alexthe666/rats/RatsMod.java
+++ src/main/java/com/github/alexthe666/rats/RatsMod.java
@@ -37,7 +37,7 @@ import java.util.List;
 public class RatsMod {
     public static final String MODID = "rats";
     public static final String NAME = "Rats";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
     public static final String LLIBRARY_VERSION = "1.7.9";
     public static CreativeTabs TAB;
     @Mod.Instance(value = MODID)
