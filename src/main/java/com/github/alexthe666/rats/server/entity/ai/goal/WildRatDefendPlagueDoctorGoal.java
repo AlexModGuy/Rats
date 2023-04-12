a333a4e2ca177ef16904f12550240b163cd6226a
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2023-04-12 09:12:29 -0600

    rats will now defend wandering plague doctors

---
+++ src/main/java/com/github/alexthe666/rats/server/entity/ai/goal/WildRatDefendPlagueDoctorGoal.java
@@ -0,0 +1,49 @@
+package com.github.alexthe666.rats.server.entity.ai.goal;
+
+import com.github.alexthe666.rats.server.entity.rat.Rat;
+import com.github.alexthe666.rats.server.entity.villager.PlagueDoctor;
+import net.minecraft.world.Difficulty;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.goal.Goal;
+import net.minecraft.world.entity.ai.goal.target.TargetGoal;
+import net.minecraft.world.entity.ai.targeting.TargetingConditions;
+
+import java.util.EnumSet;
+
+public class WildRatDefendPlagueDoctorGoal extends TargetGoal {
+	private final Rat rat;
+	private LivingEntity ownerLastHurtBy;
+	private int timestamp;
+
+	public WildRatDefendPlagueDoctorGoal(Rat rat) {
+		super(rat, false);
+		this.rat = rat;
+		this.setFlags(EnumSet.of(Goal.Flag.TARGET));
+	}
+
+	public boolean canUse() {
+		if (this.rat.hasPlague() || !this.rat.isLeashed() || this.rat.getLevel().getCurrentDifficultyAt(this.rat.blockPosition()).getDifficulty() == Difficulty.PEACEFUL) {
+			return false;
+		} else {
+			Entity entity = this.rat.getLeashHolder();
+			if (!(entity instanceof PlagueDoctor doctor)) {
+				return false;
+			} else {
+				this.ownerLastHurtBy = doctor.getLastHurtByMob();
+				int i = doctor.getLastHurtByMobTimestamp();
+				return i != this.timestamp && this.canAttack(this.ownerLastHurtBy, TargetingConditions.DEFAULT);
+			}
+		}
+	}
+
+	public void start() {
+		this.mob.setTarget(this.ownerLastHurtBy);
+		Entity entity = this.rat.getLeashHolder();
+		if (entity instanceof PlagueDoctor doctor) {
+			this.timestamp = doctor.getLastHurtByMobTimestamp();
+		}
+
+		super.start();
+	}
+}
