1c5febd0a6708fcbb54729ae2605b16b93af2fed
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2023-05-06 22:00:10 -0600

    improved whistle

--- src/main/java/com/github/alexthe666/rats/server/items/RatWhistleItem.java
+++ src/main/java/com/github/alexthe666/rats/server/items/RatWhistleItem.java
@@ -15,6 +15,7 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.pathfinder.Path;
 import net.minecraft.world.phys.AABB;
 
 import java.util.Iterator;
@@ -36,27 +37,19 @@ public class RatWhistleItem extends LoreTagItem {
 		ItemStack stack = player.getItemInHand(hand);
 		player.getCooldowns().addCooldown(this, 5);
 		float chunksize = 48 * RatConfig.ratFluteDistance;
-		List<Entity> list = level.getEntities(player, (new AABB(player.getX(), player.getY(), player.getZ(), player.getX() + 1.0D, player.getY() + 1.0D, player.getZ() + 1.0D)).inflate(chunksize, level.getHeight(), chunksize));
-		Iterator<Entity> itr = list.iterator();
-		int ratCount = 0;
-		while (itr.hasNext()) {
-			Entity entity = itr.next();
-			if (entity instanceof TamedRat rat) {
-				ratCount++;
-				if (rat.getHomePoint().isPresent()) {
-					BlockPos homePos = rat.getHomePoint().get().pos();
-					double dist = Math.sqrt(rat.getRatDistanceSq(homePos.getX() + 0.5D, homePos.getY() + 0.5D, homePos.getZ() + 0.5D));
-					if (dist > 2F && rat.canMove()) {
-						if (!rat.getNavigation().moveTo(homePos.getX() + 0.5D, homePos.getY() + 0.5D, homePos.getZ() + 0.5D, 1.5F) || dist > 1000F) {
-							rat.attemptTeleport(homePos.getX() + 0.5D, homePos.getY() + 1.5D, homePos.getZ() + 0.5D);
-						}
-					}
+		List<TamedRat> list = level.getEntitiesOfClass(TamedRat.class, (new AABB(player.getX(), player.getY(), player.getZ(), player.getX() + 1.0D, player.getY() + 1.0D, player.getZ() + 1.0D)).inflate(chunksize, level.getHeight(), chunksize), rat -> rat.getHomePoint().isPresent());
+		if (!level.isClientSide()) {
+			for (TamedRat rat : list) {
+				BlockPos homePos = rat.getHomePoint().get().pos();
+				double dist = Math.sqrt(rat.getRatDistanceSq(homePos.getX() + 0.5D, homePos.getY() + 0.5D, homePos.getZ() + 0.5D));
+				if (dist > 2F && rat.canMove()) {
+					rat.attemptTeleport(homePos.getX() + 0.5D, homePos.getY() + 1.5D, homePos.getZ() + 0.5D);
 				}
 			}
 		}
 		player.swing(hand);
-		player.displayClientMessage(Component.translatable("item.rats.rat_flute.rat_count", ratCount).withStyle(ChatFormatting.GRAY), true);
-		level.playSound(player, player.blockPosition(), RatsSoundRegistry.RAT_WHISTLE.get(), SoundSource.NEUTRAL, 1, 1.25F);
+		player.displayClientMessage(Component.translatable("item.rats.rat_flute.rat_count", list.size()).withStyle(ChatFormatting.GRAY), true);
+		level.playSound(player, player.blockPosition(), RatsSoundRegistry.RAT_WHISTLE.get(), SoundSource.PLAYERS, 1, 1.25F);
 
 		return InteractionResultHolder.success(stack);
 	}
