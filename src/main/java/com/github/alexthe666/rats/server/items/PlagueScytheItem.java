8d6e71159b22349764221dba80077e58ae27d583
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2023-06-24 20:38:22 -0600

    cleaned up scythe code

--- src/main/java/com/github/alexthe666/rats/server/items/PlagueScytheItem.java
+++ src/main/java/com/github/alexthe666/rats/server/items/PlagueScytheItem.java
@@ -1,26 +1,23 @@
 package com.github.alexthe666.rats.server.items;
 
 import com.github.alexthe666.rats.registry.RatsToolMaterialRegistry;
-import com.google.common.collect.HashMultimap;
-import com.google.common.collect.Multimap;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.ai.attributes.Attribute;
-import net.minecraft.world.entity.ai.attributes.AttributeModifier;
-import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.ToolAction;
+import net.minecraftforge.common.ToolActions;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
 public class PlagueScytheItem extends SwordItem {
 	public PlagueScytheItem(Item.Properties properties) {
-		super(RatsToolMaterialRegistry.PLAGUE_SCYTHE, 0,  1.0F, properties);
+		super(RatsToolMaterialRegistry.PLAGUE_SCYTHE, 3, -0.5F, properties);
 	}
 
 	@Override
@@ -29,12 +26,17 @@ public class PlagueScytheItem extends SwordItem {
 	}
 
 	@Override
-	public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlot equipmentSlot, ItemStack stack) {
-		Multimap<Attribute, AttributeModifier> multimap = HashMultimap.create();
-		if (equipmentSlot == EquipmentSlot.MAINHAND) {
-			multimap.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_UUID, "Weapon modifier", 12, AttributeModifier.Operation.ADDITION));
-			multimap.put(Attributes.ATTACK_SPEED, new AttributeModifier(BASE_ATTACK_SPEED_UUID, "Weapon modifier", -0.5, AttributeModifier.Operation.ADDITION));
-		}
-		return multimap;
+	public float getDestroySpeed(ItemStack stack, BlockState state) {
+		return 1.0F;
+	}
+
+	@Override
+	public boolean isCorrectToolForDrops(BlockState state) {
+		return false;
+	}
+
+	@Override
+	public boolean canPerformAction(ItemStack stack, ToolAction toolAction) {
+		return toolAction == ToolActions.SWORD_SWEEP;
 	}
 }
