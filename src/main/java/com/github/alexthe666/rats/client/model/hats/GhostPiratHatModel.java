3d7b7fa48ba76d736d5ef2b75e568df4c02f8647
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2023-06-28 20:41:39 -0600

    made ghost pirat hat fullbright as armor

--- src/main/java/com/github/alexthe666/rats/client/model/hats/GhostPiratHatModel.java
+++ src/main/java/com/github/alexthe666/rats/client/model/hats/GhostPiratHatModel.java
@@ -1,10 +1,17 @@
 package com.github.alexthe666.rats.client.model.hats;
 
 import com.github.alexthe666.rats.client.render.RatsRenderType;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.model.geom.ModelPart;
 
 public class GhostPiratHatModel extends PiratHatModel {
 	public GhostPiratHatModel(ModelPart root) {
 		super(root, RatsRenderType::getGlowingTranslucent);
+	}
+
+	@Override
+	public void renderToBuffer(PoseStack stack, VertexConsumer builder, int light, int overlay, float red, float green, float blue, float scale) {
+		super.renderToBuffer(stack, builder, 0xF000F0, overlay, red, green, blue, scale);
 	}
 }
